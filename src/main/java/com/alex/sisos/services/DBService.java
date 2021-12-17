b6a71d9989dc576571aee66fb7cb819f29043b7e
lequito <=>
2021-12-17 00:44:37 -0300

    atualizando o repositorio

--- src/main/java/com/alex/sisos/services/DBService.java
+++ src/main/java/com/alex/sisos/services/DBService.java
@@ -25,7 +25,7 @@ public class DBService {
 	private TecnicoRepository tecnicoRepository;
 
 	public void instaciadb() {
-		/*Tecnico t2 = new Tecnico(null, "Linus", "586.138.922-56", "46545651231");
+		Tecnico t2 = new Tecnico(null, "Linus", "586.138.922-56", "46545651231");
 		Tecnico t1 = new Tecnico(null, "asdf", "378.086.810-59", "123456");
 		Cliente c3 = new Cliente(null, "cliente", "678.927.782-09", "4444464654");
 		OS os3 = new OS(null, Prioridade.BAIXA, "hkgl", Status.ABERTO, t1, c3);
@@ -33,7 +33,7 @@ public class DBService {
 		tecnicoRepository.saveAll(Arrays.asList(t1, t2));
 		clienteRepository.saveAll(Arrays.asList(c3));
 		osRepository.saveAll(Arrays.asList(os3));
-*/
+
 	}
 
 }
